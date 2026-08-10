d07445e38f58dcda9c2d74953afd7d4876f42bd9
a314159265 <abhash.subedi@renegadeinsurance.com>
2026-08-10 16:45:54 +0545

    done module 2

---
+++ module_2.sql
@@ -0,0 +1,9 @@
+use my_assignment;
+
+SELECT * FROM products_task;
+
+SELECT product_name, price FROM products_task;
+
+SELECT price - cost AS unit_profit FROM products_task;
+
+SELECT name AS customer, country AS market FROM customers_task;
